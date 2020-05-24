7432dad22d149c52022f512af6f6d1d22d5e9d0e
expert-m <m.sulyak@gmail.com>
2020-05-24 18:37:33 +0700

    Update version

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 if __name__ == '__main__':
     setup(
         name='aiohttp-rpc',
-        version='0.0.1',
+        version='0.1.0',
         author='Michael Sulyak',
         url='https://github.com/expert-m/aiohttp-rpc/',
         author_email='michael@sulyak.info',
@@ -20,16 +21,16 @@ if __name__ == '__main__':
         python_requires='>=3.5',
         packages=find_packages(),
         classifiers=(
-            #"Development Status :: 1 - Planning",
-            "Development Status :: 2 - Pre-Alpha",
-            # "Development Status :: 3 - Alpha",
-            # "Development Status :: 4 - Beta",
-            # "Development Status :: 5 - Production/Stable",
-            "Environment :: Web Environment",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development :: Libraries :: Python Modules",
+            # 'Development Status :: 1 - Planning',
+            # 'Development Status :: 2 - Pre-Alpha',
+            'Development Status :: 3 - Alpha',
+            # 'Development Status :: 4 - Beta',
+            # 'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Software Development :: Libraries :: Python Modules',
         ),
     )
