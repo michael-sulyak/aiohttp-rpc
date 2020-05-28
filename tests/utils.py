c4c4a6b9c105b927bdad7b0c7b913d113ec81dda
expert-m <michael@sulyak.info>
2020-05-28 18:07:31 +0700

    Add func make_ws_client

--- tests/utils.py
+++ tests/utils.py
@@ -6,5 +6,11 @@ import aiohttp_rpc
 
 async def make_client(aiohttp_client, rpc_server: aiohttp_rpc.JsonRpcServer) -> aiohttp.ClientSession:
     app = web.Application()
-    app.router.add_post('/rpc', rpc_server.handle_request)
+    app.router.add_post('/rpc', rpc_server.handle_http_request)
+    return await aiohttp_client(app)
+
+
+async def make_ws_client(aiohttp_client, rpc_server: aiohttp_rpc.WsJsonRpcServer) -> aiohttp.ClientSession:
+    app = web.Application()
+    app.router.add_get('/rpc', rpc_server.handle_http_request)
     return await aiohttp_client(app)
