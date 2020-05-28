1219e27b6b2c1bfd63cae0c01e926b18aa9ee16a
expert-m <michael@sulyak.info>
2020-05-28 18:46:03 +0700

    Update test

--- tests/test_ws.py
+++ tests/test_ws.py
@@ -28,22 +28,19 @@ async def test_args(aiohttp_client):
 @pytest.mark.asyncio
 async def test_batch(aiohttp_client):
     def method_1(a=1):
-        return [1, 2, a]
+        return [1, a]
 
     def method_2():
-        return [1]
+        return 1
 
     rpc_server = aiohttp_rpc.WsJsonRpcServer()
-    rpc_server.add_methods((
-        method_1,
-        method_2,
-    ))
+    rpc_server.add_methods((method_1, method_2,))
 
     client = await utils.make_ws_client(aiohttp_client, rpc_server)
 
     async with aiohttp_rpc.WsJsonRpcClient('/rpc', session=client) as rpc:
-        assert await rpc.batch(('method_1', 'method_2',)) == [[1, 2, 1], [1]]
-        assert await rpc.batch((('method_1', 4), ('method_1', [], {'a': 5}),)) == [[1, 2, 4], [1, 2, 5]]
+        assert await rpc.batch(('method_1', 'method_2',)) == [[1, 1], 1]
+        assert await rpc.batch((('method_1', 4), ('method_1', [], {'a': 5}),)) == [[1, 4], [1, 5]]
 
     async with aiohttp_rpc.WsJsonRpcClient('/rpc', session=client) as rpc:
         assert await rpc.batch_notify(('method_1', 'method_2',)) is None
