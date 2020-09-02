33520d97e4f37894d550b83fc144c822db0550e7
Michael Sulyak <6431441+expert-m@users.noreply.github.com>
2020-09-02 20:51:09 +0700

    Maintain Python 3.6 compatibility

--- aiohttp_rpc/server/websocket.py
+++ aiohttp_rpc/server/websocket.py
@@ -34,11 +34,12 @@ class WsJsonRpcServer(BaseJsonRpcServer):
 
         async for ws_msg in ws:
             if ws_msg.type == http_websocket.WSMsgType.TEXT:
-                asyncio.create_task(self._handle_ws_msg(
+                coro = self._handle_ws_msg(
                     ws=ws,
                     http_request=http_request,
                     ws_msg=ws_msg,
-                ))
+                )
+                asyncio.ensure_future(coro)  # asyncio.create_task(coro) in Python 3.7+
             elif ws_msg.type == http_websocket.WSMsgType.ERROR:
                 break
 
